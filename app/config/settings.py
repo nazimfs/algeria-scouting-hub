68b0f894a0ac08a1757712eada679d96e4eac6fa
nazimfs <nazimrekik@gmail.com>
2026-07-04 22:12:44 +0200

    Add project configuration

---
+++ app/config/settings.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+# Charge le fichier .env
+load_dotenv()
+
+DB_CONFIG = {
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "database": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+}
